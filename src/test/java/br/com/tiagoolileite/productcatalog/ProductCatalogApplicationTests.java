ca93b80b9f70c2afea71c732a2fd85c860d4fe25
tiagoolileite <tiago.olileite@gmail.com>
2026-01-10 18:46:31 -0300

    Test application

--- src/test/java/br/com/tiagoolileite/productcatalog/ProductCatalogApplicationTests.java
+++ src/test/java/br/com/tiagoolileite/productcatalog/ProductCatalogApplicationTests.java
@@ -1,8 +1,15 @@
 package br.com.tiagoolileite.productcatalog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+@ExtendWith(OutputCaptureExtension.class)
 @SpringBootTest
 class ProductCatalogApplicationTests {
 
@@ -10,4 +17,14 @@ class ProductCatalogApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testMain(CapturedOutput output) {
+		String[] args = {"--spring.main.web-application-type=none"};
+		// ensure main does not throw and capture startup logs
+		assertDoesNotThrow(() -> ProductCatalogApplication.main(args));
+
+		assertThat(output.getOut())
+			.contains("Application starting - entering main")
+			.contains("Started ProductCatalogApplication");
+	}
 }
